84c0b3d3c3b37b83f7bdd25e6af1110956e6b10e
Luka Gurdulic <phallondev@gmail.com>
2023-07-06 13:03:28 +0200

    Add discord login functionality

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { supabase } from "../lib/helper/supabaseClient";
+import { useNavigate } from "react-router-dom";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
 const Login = () => {
-  return <div>loginPage</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event) => {
+      if (event === "SIGNED_IN") {
+        navigate("/success");
+      } else {
+        navigate("/");
+      }
+    });
+  }, []);
+
+  return (
+    <div className="w-[95%] mx-auto max-w-[700px] h-screen flex items-center justify-center">
+      <Auth
+        supabaseClient={supabase}
+        theme="dark"
+        providers={["discord", "google", "github"]}
+        appearance={{ theme: ThemeSupa }}
+      />
+    </div>
+  );
 };
 
 export default Login;
